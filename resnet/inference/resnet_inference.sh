9441094427d335883c15ff6085b2bce4544020f8
henrygaston <hngaston@gmail.com>
2026-04-13 11:11:10 -0700

    bug fix

--- resnet/inference/resnet_inference.sh
+++ resnet/inference/resnet_inference.sh
@@ -15,7 +15,7 @@
 #SBATCH --mail-type=FAIL
 
 #SBATCH --output=logs/slurm-%j.out
-#SBATCH --error=logs/slurm-%j.err
+#SBATCH --error=logs/slurm-%j.errd
 
 mkdir -p logs
 
@@ -23,5 +23,5 @@ cd /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/inference
 
 /resnick/groups/CS156b/from_central/2026/haa/hgaston/miniconda3/envs/cs156b/bin/python resnet_inference.py \
     --checkpoint /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/checkpoints/best_model.pt \
-    --csv /resnick/groups/CS156b/from_central/2026/haa/resnet_data/preprocessed_test_labels.csv
+    --csv /resnick/groups/CS156b/from_central/2026/haa/resnet_data/preprocessed_test_labels.csv \
     --output /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/inference/submission.csv
