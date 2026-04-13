978f908f76e879b40a1f4f9ee8e60bcc27fa8dd8
henrygaston <hngaston@gmail.com>
2026-04-13 14:32:19 -0700

    updating paths

--- resnet/inference/resnet_inference.sh
+++ resnet/inference/resnet_inference.sh
@@ -22,6 +22,6 @@ mkdir -p logs
 cd /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/inference
 
 /resnick/groups/CS156b/from_central/2026/haa/hgaston/miniconda3/envs/cs156b/bin/python resnet_inference.py \
-    --checkpoint /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/checkpoints/best_model.pt \
+    --checkpoint /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/checkpoints_resnet_v2/best_model.pt \
     --csv /resnick/groups/CS156b/from_central/2026/haa/resnet_data/preprocessed_test_labels.csv \
     --output /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/inference/submission.csv
