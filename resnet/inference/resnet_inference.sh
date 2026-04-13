55e39c463d0ca09d34b23773966a97b659779135
henrygaston <hngaston@gmail.com>
2026-04-13 01:09:37 -0700

    bug fix

--- resnet/inference/resnet_inference.sh
+++ resnet/inference/resnet_inference.sh
@@ -22,6 +22,6 @@ mkdir -p logs
 cd /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/inference
 
 /resnick/groups/CS156b/from_central/2026/haa/hgaston/miniconda3/envs/cs156b/bin/python resnet_inference.py \
-    --checkpoint /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/checkpoints/best_model.pt
+    --checkpoint /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/checkpoints/best_model.pt \
     --csv /resnick/groups/CS156b/from_central/2026/haa/preprocessed_test_labels.csv \
     --output submission.csv
