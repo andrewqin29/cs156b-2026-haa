57f432a8c80b0b5b62a0e65a50e3f1fd8cac67a6
henrygaston <hngaston@gmail.com>
2026-04-13 12:05:50 -0700

    updating paths

--- resnet/train/finetune_resnet50.sh
+++ resnet/train/finetune_resnet50.sh
@@ -14,16 +14,17 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-#SBATCH --output=logs/slurm-%j.out
-#SBATCH --error=logs/slurm-%j.err
+#SBATCH --output=/resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/logs/slurm-%j.out
+#SBATCH --error=/resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/logs/slurm-%j.err
 
-mkdir -p logs
+mkdir -p /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/logs
 
-cd /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet
+cd /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/train
+
 
 /resnick/groups/CS156b/from_central/2026/haa/hgaston/miniconda3/envs/cs156b/bin/python finetune_resnet50.py \
     --epochs 20 \
     --batch_size 32 \
     --lr 5e-5 \
-    --output_dir checkpoints_resnet_v2 \
+    --output_dir /resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/checkpoints_resnet_v2
     --csv /resnick/groups/CS156b/from_central/2026/haa/resnet_data/preprocessed_labels_v2.csv
