ec71320b48b1100e4af9daa6979b366b3a7f9db1
henrygaston <hngaston@gmail.com>
2026-04-15 00:27:05 -0700

    paths

--- resnet/inference/resnet_inference.py
+++ resnet/inference/resnet_inference.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from tqdm import tqdm
 
-sys.path.append("/resnick/groups/CS156b/from_central/2026/haa/cs156b-2026-haa/resnet/train")
+sys.path.append("/resnick/groups/CS156b/from_central/2026/haa/hgaston/cs156b-2026-haa/resnet/train")
 from finetune_resnet50 import build_model, LABEL_COLS
 
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
